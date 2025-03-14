0c143071b263905c2eb2050675731ac2ce202d38
jedcrisp <59922178+jedcrisp@users.noreply.github.com>
2025-03-14 00:45:32 -0500

    Update firebase.js

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
+const firebase = {
   apiKey: "AIzaSyA3FN_gLEYuQMOip49SLrFnfdXPAU9orp0",
   authDomain: "waypoint-auth.firebaseapp.com",
   projectId: "waypoint-auth",
@@ -12,7 +12,7 @@ const firebaseConfig = {
   measurementId: "G-TPQS4R0ZFT"
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebase) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
