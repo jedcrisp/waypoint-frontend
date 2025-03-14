22c3826d4b89910cb2af121755a92cfb914fa075
jedcrisp <59922178+jedcrisp@users.noreply.github.com>
2025-03-13 20:17:32 -0500

    Update DCAPEligibilityTest.jsx

--- frontend/src/pages/DCAPEligibilityTest.jsx
+++ frontend/src/pages/DCAPEligibilityTest.jsx
@@ -159,11 +159,11 @@ const DCAPEligibilityTest = () => {
                 }`}
               >
                 {result["DCAP Eligibility Test Result"] ?? "N/A"}
-              </span>
+            </span>
             </p>
           </div>
         </div>
-      )
+      )}
     </div>
   );
 };
