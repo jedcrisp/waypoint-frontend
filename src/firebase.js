b38adb0634999654524542260ef83fdb2f98388e
jedcrisp <59922178+jedcrisp@users.noreply.github.com>
2025-03-30 20:28:37 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // src/firebase.js
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
