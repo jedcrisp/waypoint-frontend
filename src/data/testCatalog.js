dd038cdf6adfedfa8405545ae5222083d53b899a
jedcrisp <59922178+jedcrisp@users.noreply.github.com>
2025-04-08 23:07:47 -0500

    Create testCatalog.js

---
+++ src/data/testCatalog.js
@@ -0,0 +1,28 @@
+const testCatalog = [
+  {
+    id: "acpTest",
+    name: "ACP Test",
+    price: 2999,
+    description: "Average Contribution Percentage test",
+  },
+  {
+    id: "adpTest",
+    name: "ADP Test",
+    price: 2999,
+    description: "Actual Deferral Percentage test",
+  },
+  {
+    id: "cafeteriaTest",
+    name: "Cafeteria Plan Test",
+    price: 1999,
+    description: "Section 125 Compliance for Cafeteria Plans",
+  },
+  {
+    id: "dcapEligibility",
+    name: "DCAP Eligibility Test",
+    price: 1499,
+    description: "Eligibility test for Dependent Care Assistance Program",
+  },
+];
+
+export default testCatalog;
