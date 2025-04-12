246bc8a13ff712698fdc0ed0bca89d23c1742155
jedcrisp <59922178+jedcrisp@users.noreply.github.com>
2025-04-11 22:25:17 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
 import "virtual:windi.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";  // Make sure this is here!
 import App from "./App";
 import { AuthProvider } from './AuthContext';
-import { app } from "./firebase"; // Import Firebase
+import { app } from "./firebase";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import this
+import 'react-toastify/dist/ReactToastify.css';
+
 
 console.log("Firebase App Initialized:", app);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
+    <BrowserRouter> {/* ✅ Wrap in router context */}
+      <AuthProvider>
         <App />
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
